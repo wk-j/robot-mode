c48cc580e61eb4500fb63f0dd53c53745745a5a5
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-14 18:31:20 +0700

    Added headers

--- robot-mode.el
+++ robot-mode.el
@@ -1,3 +1,31 @@
+;;; robot-mode.el --- Major mode for vue component based on web-mode and mmm-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016 codefalling, Adam Niederer
+
+;; Author: codefalling <code.falling@gmail.com>
+;; Keywords: languages
+
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (defgroup robot-mode nil
   "Robot Framework major mode"
   :link '(url-link "https://github.com/wingyplus/robot-mode")
